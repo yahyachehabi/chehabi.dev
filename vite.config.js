b96599ab37e5336577f663c81d4ce16091e31af4
yahyachehabi <yahyachehabi@users.noreply.github.com>
2021-08-07 18:27:27 +0100

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,9 @@ export default {
         // Add every html file respecting folder structure of your app
         index: resolve(__dirname ,'index.html'),
         indexEn: resolve(__dirname ,'index-en.html'),
-        message: resolve(__dirname, 'message-confirmation.html'),
-        messageEn: resolve(__dirname, 'message-confirmation-en.html'),
+        message: resolve(__dirname, 'merci.html'),
+        messageEn: resolve(__dirname, 'thankyou.html'),
+        test: resolve(__dirname, 'projects/html/test.html'),
         // third: resolve(__dirname, 'third.html'),
         // fourth: resolve(__dirname, 'nested/fourth.html')
 
