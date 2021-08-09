d19f51b7155f19aa8eda22d58449e42318a06eec
yahyachehabi <yahyachehabi@users.noreply.github.com>
2021-08-09 01:00:21 +0100

    Fix code color with image

--- projects/html/projet-1.md
+++
@@ -1,7 +0,0 @@
-## Code du projet
-```html
-    <div class="animate-spin ">
-        <div class="bg-red-500  w-50 h-50 rounded-full opacity-60 -mb-20"></div>
-        <div class="bg-blue-500  w-50 h-50 rounded-full opacity-60 -mt-20"></div>
-    </div>
-```
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,20 @@
-import {resolve} from 'path'
 import WindiCSS from 'vite-plugin-windicss'
+import path from "path";
+import glob from "glob";
 
 export default {
   plugins: [
     WindiCSS(),
   ],
-
   server: {
     open: '/',
     port: 3636
   },
-
+  root: __dirname,
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: {
-        // Add every html file respecting folder structure of your app
-        index: resolve(__dirname ,'index.html'),
-        indexEn: resolve(__dirname ,'index-en.html'),
-        message: resolve(__dirname, 'merci.html'),
-        messageEn: resolve(__dirname, 'thankyou.html'),
-        test: resolve(__dirname, 'projects/test.html'),
-        projet1: resolve(__dirname, 'projects/html/projet-1.html'),
-        projet1Md: resolve(__dirname, 'projects/html/projet-1.md'),
-        // third: resolve(__dirname, 'third.html'),
-        // fourth: resolve(__dirname, 'nested/fourth.html')
-
-      }
-    }
-  }
+        outDir: path.join(__dirname, "build"),
+        rollupOptions: {
+          input: glob.sync(path.resolve(__dirname, "**/*.html")),
+        },
+    },
 }
